cbee41df51521c2d97d5671b6cfafbc5d3d66a34
Gavin Chappell <2798739+g-a-c@users.noreply.github.com>
2023-03-25 07:11:59 +0000

    a

--- device42/provider.go
+++ device42/provider.go
@@ -1,95 +1,151 @@
-package device42
+package provider
 
 import (
 	"context"
 	"crypto/tls"
-	"errors"
-	"fmt"
 	"net/http"
 	"os"
 
-	resty "github.com/go-resty/resty/v2"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/provider"
+	"github.com/hashicorp/terraform-plugin-framework/provider/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
-func Provider() *schema.Provider {
-	return &schema.Provider{
-		Schema: map[string]*schema.Schema{
-			"hostname": &schema.Schema{
-				Type:        schema.TypeString,
-				Optional:    true,
+// Ensure Device42Provider satisfies various provider interfaces.
+var _ provider.Provider = &Device42Provider{}
+
+// Device42Provider defines the provider implementation.
+type Device42Provider struct {
+	// version is set to the provider version on release, "dev" when the
+	// provider is built and ran locally, and "test" when running acceptance
+	// testing.
+	version string
+}
+
+// Device42ProviderModel describes the provider data model.
+type Device42ProviderModel struct {
+	Hostname types.String `tfsdk:"hostname"`
+	Username types.String `tfsdk:"username"`
+	Password types.String `tfsdk:"password"`
+	Insecure types.Bool   `tfsdk:"insecure"`
+}
+
+func (p *Device42Provider) Metadata(ctx context.Context, req provider.MetadataRequest, resp *provider.MetadataResponse) {
+	resp.TypeName = "device42"
+	resp.Version = p.version
+}
+
+func (p *Device42Provider) Schema(ctx context.Context, req provider.SchemaRequest, resp *provider.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		Attributes: map[string]schema.Attribute{
+			"hostname": schema.StringAttribute{
 				Description: "hostname of the Device42 instance",
-				DefaultFunc: func() (interface{}, error) {
-					if v := os.Getenv("D42_HOSTNAME"); v != "" {
-						return v, nil
-					}
-
-					return "swaggerdemo.device42.com", nil
-				},
-			},
-			"username": &schema.Schema{
-				Type:        schema.TypeString,
-				Required:    true,
-				Description: "username with access to the Device42 instance",
-				DefaultFunc: func() (interface{}, error) {
-					if v := os.Getenv("D42_USERNAME"); v != "" {
-						return v, nil
-					}
-
-					return nil, errors.New("no username was provided via the D42_USERNAME environment variable")
-				},
-			},
-			"password": &schema.Schema{
-				Type:        schema.TypeString,
-				Required:    true,
-				Description: "password for the username with access",
-				DefaultFunc: func() (interface{}, error) {
-					if v := os.Getenv("D42_PASSWORD"); v != "" {
-						return v, nil
-					}
-
-					return nil, errors.New("no password was provided via the D42_PASSWORD environment variable")
-				},
-			},
-			"insecure": &schema.Schema{
-				Type:        schema.TypeBool,
 				Optional:    true,
-				Description: "disable TLS verification",
-				DefaultFunc: func() (interface{}, error) {
-					if v := os.Getenv("D42_INSECURE"); v == "true" {
-						return true, nil
-					}
-
-					return false, nil
-				},
+			},
+			"username": schema.StringAttribute{
+				Description: "username with access to the Device42 instance",
+				Required:    true,
+			},
+			"password": schema.StringAttribute{
+				Description: "password for the username with access",
+				Required:    true,
+				Sensitive:   true,
+			},
+			"insecure": schema.BoolAttribute{
+				Description: "disable TLS verification - not recommended",
+				Optional:    true,
 			},
 		},
-		ResourcesMap: map[string]*schema.Resource{},
-		DataSourcesMap: map[string]*schema.Resource{
-			"device42_password": dataSourcePassword(),
-		},
-		ConfigureContextFunc: configureProvider,
 	}
 }
 
-func configureProvider(ctx context.Context, d *schema.ResourceData) (m interface{}, diags diag.Diagnostics) {
-	client := resty.New()
-	if d.Get("insecure").(bool) == true {
-		diags = append(diags, diag.Diagnostic{
-			Severity: diag.Warning,
-			Summary:  "Insecure mode enabled",
-			Detail:   "TLS certificate verification was disabled for this session",
-		})
-		insecureTransport := &http.Transport{
+func (p *Device42Provider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
+	apiHostname := ""
+	username := ""
+	password := ""
+
+	var data Device42ProviderModel
+
+	// Configuration values are now available.
+	// if data.Endpoint.IsNull() { /* ... */ }
+
+	resp.Diagnostics.Append(req.Config.Get(ctx, &data)...)
+
+	if v := os.Getenv("D42_HOSTNAME"); v != "" {
+		apiHostname = v
+	} else {
+		apiHostname = data.Hostname.ValueString()
+	}
+
+	if v := os.Getenv("D42_USERNAME"); v != "" {
+		username = v
+	} else {
+		username = data.Username.ValueString()
+	}
+
+	if v := os.Getenv("D42_PASSWORD"); v != "" {
+		password = v
+	} else {
+		password = data.Password.ValueString()
+	}
+
+	if apiHostname == "" {
+		resp.Diagnostics.AddError(
+			"Missing hostname",
+			"No hostname was provided",
+		)
+	}
+	if username == "" {
+		resp.Diagnostics.AddError(
+			"Missing username",
+			"No username was provided",
+		)
+	}
+	if password == "" {
+		resp.Diagnostics.AddError(
+			"Missing password",
+			"No password was provided",
+		)
+	}
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	client := http.DefaultClient
+	if data.Insecure.ValueBool() == true {
+		// disable TLS verification - not recommended
+		resp.Diagnostics.AddWarning(
+			"Insecure mode enabled",
+			"TLS verification was disabled for this session.",
+		)
+		client.Transport = &http.Transport{
 			TLSClientConfig: &tls.Config{
 				InsecureSkipVerify: true,
 			},
 		}
-		client.SetTransport(insecureTransport)
 	}
-	client.SetHostURL(fmt.Sprintf("https://%s", d.Get("hostname").(string)))
-	client.SetBasicAuth(d.Get("username").(string), d.Get("password").(string))
+	resp.DataSourceData = client
+	resp.ResourceData = client
+}
 
-	return client, diags
+func (p *Device42Provider) Resources(ctx context.Context) []func() resource.Resource {
+	return []func() resource.Resource{}
+}
+
+func (p *Device42Provider) DataSources(ctx context.Context) []func() datasource.DataSource {
+	return []func() datasource.DataSource{
+		D42PasswordDataSource,
+	}
+}
+
+func New(version string) func() provider.Provider {
+	return func() provider.Provider {
+		return &Device42Provider{
+			version: version,
+		}
+	}
 }
---
+++ device42_backup/provider.go
@@ -0,0 +1,95 @@
+package device42
+
+import (
+	"context"
+	"crypto/tls"
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+
+	resty "github.com/go-resty/resty/v2"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func Provider() *schema.Provider {
+	return &schema.Provider{
+		Schema: map[string]*schema.Schema{
+			"hostname": &schema.Schema{
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "hostname of the Device42 instance",
+				DefaultFunc: func() (interface{}, error) {
+					if v := os.Getenv("D42_HOSTNAME"); v != "" {
+						return v, nil
+					}
+
+					return "swaggerdemo.device42.com", nil
+				},
+			},
+			"username": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "username with access to the Device42 instance",
+				DefaultFunc: func() (interface{}, error) {
+					if v := os.Getenv("D42_USERNAME"); v != "" {
+						return v, nil
+					}
+
+					return nil, errors.New("no username was provided via the D42_USERNAME environment variable")
+				},
+			},
+			"password": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "password for the username with access",
+				DefaultFunc: func() (interface{}, error) {
+					if v := os.Getenv("D42_PASSWORD"); v != "" {
+						return v, nil
+					}
+
+					return nil, errors.New("no password was provided via the D42_PASSWORD environment variable")
+				},
+			},
+			"insecure": &schema.Schema{
+				Type:        schema.TypeBool,
+				Optional:    true,
+				Description: "disable TLS verification",
+				DefaultFunc: func() (interface{}, error) {
+					if v := os.Getenv("D42_INSECURE"); v == "true" {
+						return true, nil
+					}
+
+					return false, nil
+				},
+			},
+		},
+		ResourcesMap: map[string]*schema.Resource{},
+		DataSourcesMap: map[string]*schema.Resource{
+			"device42_password": dataSourcePassword(),
+		},
+		ConfigureContextFunc: configureProvider,
+	}
+}
+
+func configureProvider(ctx context.Context, d *schema.ResourceData) (m interface{}, diags diag.Diagnostics) {
+	client := resty.New()
+	if d.Get("insecure").(bool) == true {
+		diags = append(diags, diag.Diagnostic{
+			Severity: diag.Warning,
+			Summary:  "Insecure mode enabled",
+			Detail:   "TLS certificate verification was disabled for this session",
+		})
+		insecureTransport := &http.Transport{
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: true,
+			},
+		}
+		client.SetTransport(insecureTransport)
+	}
+	client.SetHostURL(fmt.Sprintf("https://%s", d.Get("hostname").(string)))
+	client.SetBasicAuth(d.Get("username").(string), d.Get("password").(string))
+
+	return client, diags
+}
